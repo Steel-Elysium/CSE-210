eebb0d94bbeb19fbb1e4be6113e7bba39fa04327
Steel Elysium <gregelbl@gmail.com>
2023-04-24 11:26:33 -0600

    Week 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        string first_name, last_name;
+        Console.WriteLine("Hello Prep1 World!\n");
+        Console.Out.Write("What is your First name? ");
+        first_name = Console.In.ReadLine();
+        Console.Out.Write("What is your last name? ");
+        last_name = Console.In.ReadLine();
+        Console.Out.WriteLine($"\nYou are {last_name}, {first_name} {last_name}");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int grade_number;
+        string grade_letter, usr_in;
+        Console.WriteLine("Hello Prep2 World!\n");
+        Console.Out.Write("What is the Grade number? ");
+        usr_in = Console.In.ReadLine();
+        grade_number = int.Parse(usr_in);
+        grade_letter = "";
+        if(grade_number / 10 >= 9){
+            grade_letter += "A";
+        }
+        else if( grade_number / 10 == 8){
+            grade_letter += "B";
+        }
+        else if(grade_number / 10 == 7){
+            grade_letter += "C";
+        }
+        else if (grade_number / 10 == 6){
+            grade_letter += "D";
+        }
+        else{
+            grade_letter += "F";
+        }
+        if (grade_number % 10 < 3){
+            grade_letter += "-";
+        }
+        else if(grade_number % 10 >= 7){
+            grade_letter += '+';
+        }
+        else{
+            grade_letter += "";
+        }
+        Console.Out.WriteLine($"Your Grade of {grade_number} is a {grade_letter}!");
     }
 }
