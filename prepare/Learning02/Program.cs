a1912aee8fb3c19a098ae5fbc9417f0d6303f4f6
Steel Elysium <gregelbl@gmail.com>
2023-05-02 09:57:18 -0600

    comments

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -16,27 +16,28 @@ class Program
     }
 }
 class Job{
+    
     private string companyName, jobTitle;
     private int startYear, endYear;
-    
+    private protected int currentYear = 2023; // this is th current year Change every year
     //Constructors
     public Job(){
         this.companyName = "Not a Name";
         this.jobTitle = null;
-        this.startYear = 2023;
-        this.endYear = 2023;
+        this.startYear = this.currentYear;
+        this.endYear = this.currentYear;
     }
     public Job(string company){
         this.companyName = company;
         this.jobTitle = null;
-        this.startYear = 2023;
-        this.endYear = 2023;
+        this.startYear = this.currentYear;
+        this.endYear = this.currentYear;
     }
     public Job(string company, int startYear){
         this.companyName = company;
         this.jobTitle = null;
         this.startYear = startYear;
-        this.endYear = 2023;
+        this.endYear = this.currentYear;
     }
     public Job(string company, int startYear, int endYear){
         this.companyName = company;
@@ -47,14 +48,14 @@ class Job{
     public Job(string company, string title){
         this.companyName = company;
         this.jobTitle = title;
-        this.startYear = 2023;
-        this.endYear = 2023;
+        this.startYear = this.currentYear;
+        this.endYear = this.currentYear;
     }
     public Job(string company, string title, int startYear){
         this.companyName = company;
         this.jobTitle = title;
         this.startYear = startYear;
-        this.endYear = 2023;
+        this.endYear = this.currentYear;
     }
     public Job(string company, string title, int startYear, int endYear){
         this.companyName = company;
@@ -64,28 +65,60 @@ class Job{
     }
 
     public string GetCompanyName(){
+        /*
+        @pram: None
+        @return: string, the String of the comony name
+        */
         return this.companyName;
     }
     public string GetJobTitle(){
+        /*
+        @pram: None
+        @return: string, The Job Title
+        */
         return this.jobTitle;
     }
     public int GetStartYear(){
+        /*
+        @pram: None
+        @return: int, The Start Year value
+        */
         return this.startYear;
     }
     public int GetEndYear(){
+        /*
+        @pram: None
+        @retrurn: int, The End Year value
+        */
         return this.endYear;
     }
 
     public void SetCompanyName(string Name){
+        /*
+        @Pram: String, What is wanted to set the Compony name to
+        @retrun: None
+        */
         this.companyName = Name;
     }
     public void SetJobTitle(string title){
+        /*
+        @Pram: String, What the Job title is wanted to be set to
+        @return: None
+        */
         this.jobTitle = title;
     }
     public void SetStartYear(int year){
+        /*
+        @pram: int, What the Start Year is being set to
+        @return: None
+        */
         this.startYear = year;
     }
     public void SetEndYear(int year){
+        /*
+        @pram: int, What the End Year is being set to
+        @retrun: None
+        */
         this.endYear = year;
     }
     public void Display(){
@@ -112,28 +145,52 @@ class Resume{
     }
 
     public void AddJobs(List<Job> newJobs){
+        /*
+        @pram: List<Job>, A list made up of Job class
+        @return: None
+        */
         foreach(Job newJob in newJobs){
             this.AddJob(newJob);
         }
     }
     public void AddJob(Job newJob){
+        /*
+        @pram: job, What Job is being added to the Resume
+        @retrun: None
+        */
         this.jobs.Add(newJob);
     }
     public List<Job> ClearJobs(){
+        /*
+        @pram: None
+        @retrun: List<Job>, the cleared List<Job>
+        */
         List<Job> oldJobs = this.jobs;
         this.jobs.Clear();
         return oldJobs;
     }
 
     public Job GetJobAtIndex(int index){
+        /*
+        @pram: int, the lookup index
+        @retrun: Job, the Job in List<Job> at the index 
+        */
         Job wantedJob;
         wantedJob = this.jobs[index];
         return wantedJob;
     }
     public List<Job> GetJobs(){
+        /*
+        @pram: None
+        @return: List<job>, retruns the List<Job>
+        */
         return this.jobs;
-    }
+    }  
     public string GetName(){
+        /*
+        @pram: None
+        @retrun: string, The name varable
+        */
         return this.name;
     }
 
@@ -141,6 +198,7 @@ class Resume{
         Console.Out.WriteLine($"Name: {this.name}");
         Console.Out.WriteLine("Jobs: ");
         foreach(Job job in jobs){
+            Console.Out.Write("    "); // 4 spaces
             job.Display();
         }
     }
